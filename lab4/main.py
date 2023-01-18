76e97425024c2d25c0a3b696ca9ce8510c386094
SemenSokolkov <92160052+SemenSokolkov@users.noreply.github.com>
2023-01-18 10:44:59 +0500

    Написаны функции для генерации списка списков
    Написаны функции для генерации списка списков с ФИО и зарплатой человека и функция для записи данных в .csv файл

--- lab4/main.py
+++ lab4/main.py
@@ -1,9 +1,15 @@
+import csv;
+import random;
+FILENAME : str = "data_set.csv"; #имя изначального файла с данными
+OUTPUTFILE : str = "new_data_set.csv"; #имя преобразованного файла с данными
+#переопределённый map
 def OverrideMap(func, iterable : list) -> list:
     resultList = list();
     for obj in iterable:
         resultList.append(func(obj));
     return resultList;
 
+#переопределённый reduce
 def OverrideReduce(func, iterable : list):
     index : int = 2;
     length : int = len(iterable);
@@ -13,6 +19,26 @@ def OverrideReduce(func, iterable : list):
         index += 1;
     return result;
 
-if __name__ == "__main__":
-    print(OverrideMap(lambda x: x * 2 + 3, [10, 15, 21, 33, 42, 55]))
-    print(OverrideReduce(lambda x, y: x + y, [1]))
+#Генерация списка списков с ФИО и зарплатой человека
+def GenerateDataSet() -> list:
+    Names : list = ["Денис", "Вячеслав", "Анатолий", "Олег", "Павел"];
+    Surnames : list = ["Мельников", "Глазырин", "Омутных", "Гостев"];
+    Patronymics : list = ["Денисович", "Вячеславович", "Анатольевич", "Олегович", "Павлович"];
+    Persons = list();
+    i : int = 0;
+    count : int = 50;
+    Persons.append(["ФИО", "Зарплата"]);
+    while i < count:
+        randomFullName = Surnames[random.randint(0 , len(Surnames) - 1)] + " " + Names[random.randint(0 , len(Names) - 1)] + " "  + Patronymics[random.randint(0 , len(Patronymics) - 1)];
+        Persons.append([randomFullName, random.randint(1, 1000) * 1000]);
+        i += 1;
+    return Persons;
+   
+#Запись в .csv
+def WriteInCSV(dataSet : list, fileName : str):
+     with open(fileName, "w", newline="") as file:
+        writer = csv.writer(file, delimiter=',');
+        writer.writerows(dataSet);
+
+#основной код
+WriteInCSV(GenerateDataSet(), FILENAME);
